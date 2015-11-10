74b55b8c1c8d70e805af1c555b6c929d4f2136e0
jahpd <gcravista@gmail.com>
2015-11-10 16:06:59 -0200

    inserido bloco para adicionar webpd

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+var WEBPD_MASTER = "https://raw.githubusercontent.com/sebpiq/WebPd/master/dist/webpd-latest.min.js"
+
 module.exports = {
     blocks: {
         webpd: {
-            process: function(blk) {
+            process: function(path) {
 		return '<script type=\"text/javascript\" charset=\"utf-8\">\n\tvar patch=$.get(\''
 		    + path 
 		    + '\', function(patchStr) { patch = Pd.loadPatch(patchStr); Pd.start()})</script>'
             }
         }
+	import_webpd: {
+	    process:  function (link) {
+		return "<script src=\"+"WEBPD_MASTER+"+\"></script>"
+            }
+	}
+	    
     }
 };
