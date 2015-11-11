a9a76b6b8c8573bbcaec2cce338e8f66203945af
jahpd <gcravista@gmail.com>
2015-11-11 11:29:26 -0200

    primeira versao (estável?) de renderização de patchs

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ module.exports = {
     book: {
 	assets: './assets',
 	js: [
-	    'jquery-latest.min.js',
 	    'webpd-latest.min.js',
 	    'pd-fileutils-latest.js'
 	],
 	/* Load patchs */
 	html: {
 	    'head:end': function(){
-		return '<script src=\"gitbook/plugins/gitbook-plugin-webpd/pd-environment.js\"></script>';
+		return '<script src=\"gitbook/plugins/gitbook-plugin-webpd/jquery-latest.min.js\">\n</script>\n<script src=\"gitbook/plugins/gitbook-plugin-webpd/pd-environment.js\"></script>';
 	    },
 	    'body:end': function(){
 		return '<script type=\"text/javascript\"> for(var patch in pd_environment) { window.pd_environment[patch]() }</script>';
@@ -23,12 +22,12 @@ module.exports = {
             process: function(path) {
 		console.log(path);
 		var p = path.body.split(" ") ? path.body.split(" ")[1] : path.body;
-		var name = path.body.split("/");
-		var name = name[name.length-1];
-		var name = name.split(".pd")[0];
+		var split_path = path.body.split("/");
+		var filename = split_path[split_path.length-1];
+		var name = filename.split(".pd")[0];
 		console.log("Patch loaded: "+name);
 		console.log(p)
-		return '<div>\n<script type=\"text/javascript\">\n\twindow.pd_environment.'+name+' = function(){\n\t\t$.get(\"'+p+'\", function(patchStr) {\n\t\t\tpatch = Pd.loadPatch(patchStr);\n\t\t\tvar rendered = pdfu.renderSvg(patch, {svgFile: false});\n\t\t\t$(\'#'+name+'\').html(rendered);\n\t\t});\n\t}\n</script>\n<span>'+name+'</span>\n<div id=\''+name+'\'></div></div>'
+		return '<div>\n<script type=\"text/javascript\">\n\twindow.pd_environment.'+name+' = function(){\n\t\t$.get(\"'+p+'\", function(patchStr) {\n\t\t\tpatch = pdfu.parse(patchStr);\n\t\t\tvar rendered = pdfu.renderSvg(patch, {svgFile: false});\n\t\t\t$(\'#'+name+'\').html(rendered);\n\t\t});\n\t}\n</script>\n<span>'+filename+'</span>\n<div id=\''+name+'\'></div></div>'
             }
         }
     }
