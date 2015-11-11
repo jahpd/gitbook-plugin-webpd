fbd75ca4fa2457dc6c1a7cd6c02e93a17d9dd59f
jahpd <gcravista@gmail.com>
2015-11-11 01:01:03 -0200

    corrigidas boas partes dos problemas, agora o unico bug eh com o pd-fileutils

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-var WEBPD_MASTER = "https://raw.githubusercontent.com/sebpiq/WebPd/master/dist/webpd-latest.min.js"
 
 module.exports = {
+    /* Configure PD assets*/
+    book: {
+	assets: './assets',
+	js: [
+	    'jquery-latest.min.js',
+	    'webpd-latest.min.js',
+	    'pd-fileutils-latest.js'
+	],
+	/* Load patchs */
+	html: {
+	    'head:end': function(){
+		return '<script src=\"gitbook/plugins/gitbook-plugin-webpd/pd-environment.js\"></script>';
+	    },
+	    'body:end': function(){
+		return '<script type=\"text/javascript\"> for(var patch in pd_environment) { window.pd_environment[patch]() }</script>';
+	    }
+	}
+    },
     blocks: {
-        webpd: {
+        patch: {
             process: function(path) {
-		return '<script type=\"text/javascript\" charset=\"utf-8\">\n\tvar patch=$.get(\''
-		    + path 
-		    + '\', function(patchStr) { patch = Pd.loadPatch(patchStr); Pd.start()})</script>'
+		console.log(path);
+		var p = path.body.split(" ") ? path.body.split(" ")[1] : path.body;
+		var name = path.body.split("/");
+		var name = name[name.length-1];
+		var name = name.split(".pd")[0];
+		console.log("Patch loaded: "+name);
+		console.log(p)
+		return '<div>\n<script type=\"text/javascript\">\n\twindow.pd_environment.'+name+' = function(){\n\t\t$.get(\"'+p+'\", function(patchStr) {\n\t\t\tpatch = Pd.loadPatch(patchStr);\n\t\t\tvar rendered = pdfu.renderSvg(patch, {svgFile: false});\n\t\t\t$(\'#'+name+'\').html(rendered);\n\t\t});\n\t}\n</script>\n<span>'+name+'</span>\n<div id=\''+name+'\'></div></div>'
             }
-        },
-	import_webpd: {
-	    process:  function (link) {
-		return "<script src=\"+"WEBPD_MASTER+"+\"></script>"
-            }
-	}	    
+        }
     }
 };
+
