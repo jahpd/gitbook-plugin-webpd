0255e1d04ffa8b1d2cac5479bf02f0a22efa4d76
jahpd <gcravista@gmail.com>
2015-11-11 13:12:51 -0200

    atualizado bug com varios patchs ao mesmo tempo

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 module.exports = {
     /* Configure PD assets*/
     book: {
@@ -10,10 +9,17 @@ module.exports = {
 	/* Load patchs */
 	html: {
 	    'head:end': function(){
-		return '<script src=\"gitbook/plugins/gitbook-plugin-webpd/jquery-latest.min.js\">\n</script>\n<script src=\"gitbook/plugins/gitbook-plugin-webpd/pd-environment.js\"></script>';
+		return '<script src=\"gitbook/plugins/gitbook-plugin-webpd/jquery-latest.min.js\"></script>\n\t<script src=\"gitbook/plugins/gitbook-plugin-webpd/pd-environment.js\"></script>';
 	    },
 	    'body:end': function(){
-		return '<script type=\"text/javascript\"> for(var patch in pd_environment) { window.pd_environment[patch]() }</script>';
+		return '<script type=\"text/javascript\">\n'+
+		    '$(\'.pd_container\').click(function(){\n'+
+		    '\tcontainer = $(this).attr(\'id\');\n'+
+		    '\tpatch = container.split(\'pd_container_\')[1];\n'+
+		    '\tconsole.log(\'loading \'+patch+\'...\');\n'+
+		    '\tpd_environment[patch]();\n'+
+		    '});\n'+
+		    '</script>';
 	    }
 	}
     },
@@ -21,13 +27,12 @@ module.exports = {
         patch: {
             process: function(path) {
 		console.log(path);
-		var p = path.body.split(" ") ? path.body.split(" ")[1] : path.body;
 		var split_path = path.body.split("/");
 		var filename = split_path[split_path.length-1];
-		var name = filename.split(".pd")[0];
+		var name = filename.split(".pd")[0].toLowerCase()
 		console.log("Patch loaded: "+name);
-		console.log(p)
-		return '<div>\n<script type=\"text/javascript\">\n\twindow.pd_environment.'+name+' = function(){\n\t\t$.get(\"'+p+'\", function(patchStr) {\n\t\t\tpatch = pdfu.parse(patchStr);\n\t\t\tvar rendered = pdfu.renderSvg(patch, {svgFile: false});\n\t\t\t$(\'#'+name+'\').html(rendered);\n\t\t});\n\t}\n</script>\n<span>'+filename+'</span>\n<div id=\''+name+'\'></div></div>'
+		console.log(path.body)
+		return '<div class=\'pd_container\' id=\'pd_container_'+name+'\' >\n<script type=\"text/javascript\">\n\twindow.pd_environment.'+name+' = function(){\n\t\t$.get(\"'+path.body+'\", function(patchStr) {\n\t\t\tvar '+name+'_patch = pdfu.parse(patchStr);\n\t\t\tvar '+name+'_rendered = pdfu.renderSvg('+name+'_patch, {svgFile: false});\n\t\t\t$(\'#'+name+'\').html('+name+'_rendered);\n\t\t});\n\t}\n</script>\n<span>'+filename+' (click me)</span>\n<div id=\''+name+'\'></div></div>'
             }
         }
     }
